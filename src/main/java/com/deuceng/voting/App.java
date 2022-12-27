b78ce0c1ac0386291fc0b14a20cdf12edfb87d5e
Ozan Uslan <ozanuslan@hotmail.com>
2022-12-27 19:42:26 +0300

    Added command line demo

--- src/main/java/com/deuceng/voting/App.java
+++ src/main/java/com/deuceng/voting/App.java
@@ -1,9 +1,11 @@
 package com.deuceng.voting;
 
+import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.deuceng.voting.cmd.VotingCommandLine;
 import com.deuceng.voting.cypto.paillier.PaillierCryptoSystem;
 import com.deuceng.voting.cypto.paillier.PaillierKeyPair;
 import com.deuceng.voting.system.Candidate;
@@ -11,38 +13,60 @@ import com.deuceng.voting.system.VotingSystem;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        PaillierKeyPair keyPair = PaillierCryptoSystem.generateKeyPair(16, new SecureRandom());
-        System.out.println("Key pair generated. Bit length: " + 16);
-        System.out.println("Public key: " + keyPair.getPublicKey());
-        System.out.println("Private key: " + keyPair.getPrivateKey());
-        List<Candidate> candidates = List.of(
-                new Candidate(1, "A"),
-                new Candidate(2, "B"));
-        System.out.println("Candidates: ");
-        for (Candidate c : candidates) {
-            System.out.println(c);
-        }
+        // PaillierKeyPair keyPair = PaillierCryptoSystem.generateKeyPair(64, new
+        // SecureRandom());
+        // System.out.println("Key pair generated. Bit length: " + 16);
+        // System.out.println("Public key: " + keyPair.getPublicKey());
+        // System.out.println("Private key: " + keyPair.getPrivateKey());
+        // List<Candidate> candidates = List.of(
+        // new Candidate(1, "A"),
+        // new Candidate(2, "B"));
+        // System.out.println("Candidates: ");
+        // for (Candidate c : candidates) {
+        // System.out.println(c);
+        // }
 
-        VotingSystem vs = new VotingSystem(keyPair, 1l, candidates, 3l);
-        System.out.println("Voting system created. Seed: " + 1l + ", Voter count: " + 3l);
+        // long voters = 1000000;
+        // VotingSystem vs = new VotingSystem(keyPair, 1l, candidates, voters);
+        // System.out.println("Voting system created. Seed: " + 1l + ", Voter count: " +
+        // voters);
 
-        System.out.println("Adding vote for voter 1: candidate 1");
-        vs.addVote(1);
-        System.out.println("Adding vote for voter 2: candidate 2");
-        vs.addVote(2);
-        System.out.println("Adding vote for voter 3: candidate 1");
-        vs.addVote(1);
+        // // System.out.println("Adding vote for voter 1: candidate 1");
+        // // vs.addVote(1);
+        // // System.out.println("Adding vote for voter 2: candidate 2");
+        // // vs.addVote(2);
+        // // System.out.println("Adding vote for voter 3: candidate 1");
+        // // vs.addVote(1);
 
-        var result = vs.tallyVotes();
-        System.out.println("Votes tallied.");
+        // // PaillierCryptoSystem pcs = new PaillierCryptoSystem(new SecureRandom());
 
-        List<Candidate> sorted = result.stream()
-                .sorted((a, b) -> Long.compare(b.getVotes(), a.getVotes()))
-                .collect(Collectors.toList());
+        // // BigInteger v = pcs.encrypt(BigInteger.valueOf(1_000_000l).pow(2),
+        // // keyPair.getPublicKey());
 
-        System.out.println("Sorted results: ");
-        for (Candidate c : sorted) {
-            System.out.println(c);
-        }
+        // // System.out.println("Encrypted: " + v);
+
+        // // BigInteger d = pcs.decrypt(v, keyPair.getPrivateKey(),
+        // // keyPair.getPublicKey());
+
+        // // System.out.println("Decrypted: " + d);
+
+        // // for (int i = 0; i < voters; i++) {
+        // // vs.addVote(2);
+        // // }
+
+        // var result = vs.tallyVotes();
+        // System.out.println("Votes tallied.");
+
+        // List<Candidate> sorted = result.stream()
+        // .sorted((a, b) -> Long.compare(b.getVotes(), a.getVotes()))
+        // .collect(Collectors.toList());
+
+        // System.out.println("Sorted results: ");
+        // for (Candidate c : sorted) {
+        // System.out.println(c);
+        // }
+
+        VotingCommandLine vcl = new VotingCommandLine();
+        vcl.displayMainMenu();
     }
 }
---
+++ src/main/java/com/deuceng/voting/cmd/VotingCommandLine.java
@@ -0,0 +1,256 @@
+package com.deuceng.voting.cmd;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.deuceng.voting.cypto.paillier.PaillierCryptoSystem;
+import com.deuceng.voting.cypto.paillier.PaillierKeyPair;
+import com.deuceng.voting.system.Candidate;
+import com.deuceng.voting.system.UnknownCandidateException;
+import com.deuceng.voting.system.VotingSystem;
+
+public class VotingCommandLine {
+  private int keyPairBitLength;
+  private long voterCount;
+  private long candidateCount;
+  private List<Candidate> candidates;
+  private PaillierKeyPair keyPair;
+  private long seed;
+  private VotingSystem votingSystem;
+  private boolean isVotingComplete;
+  private Scanner sc;
+  private Random random;
+
+  public VotingCommandLine() {
+    this.isVotingComplete = false;
+    sc = new Scanner(System.in);
+  }
+
+  public void displayMainMenu() {
+    System.out.println("\n\nWelcome to the Voting System");
+    System.out.println("1. Create a new voting system");
+    System.out.println("2. Exit");
+
+    System.out.print("Choice: ");
+    String choice = sc.nextLine();
+    switch (choice) {
+      case "1":
+        clearParameters();
+        displayVotingSystemCreationMenu();
+        break;
+      case "2":
+        System.out.println("Exiting...");
+        System.exit(0);
+        break;
+      default:
+        System.out.println("Invalid choice. Please try again.");
+        displayMainMenu();
+    }
+
+    sc.close();
+  }
+
+  private void clearParameters() {
+    this.keyPairBitLength = 0;
+    this.voterCount = 0;
+    this.candidateCount = 0;
+    this.candidates = null;
+    this.keyPair = null;
+    this.seed = 0;
+    this.votingSystem = null;
+    this.isVotingComplete = false;
+
+    System.gc();
+  }
+
+  public void displayVotingSystemCreationMenu() {
+    System.out.println("\n\nVoting System Creation Menu");
+
+    System.out.print("Please enter candidate count: ");
+    // try to parse to long and check if it is positive
+    // if not, display error message and ask again
+    // if yes, continue
+    String candidateCount = sc.nextLine();
+    try {
+      long candidateCountLong = Long.parseLong(candidateCount);
+      if (candidateCountLong <= 0) {
+        System.out.println("Candidate count must be a positive integer.");
+        displayVotingSystemCreationMenu();
+      }
+      this.candidateCount = candidateCountLong;
+    } catch (NumberFormatException e) {
+      System.out.println("Candidate count must be a positive integer.");
+      displayVotingSystemCreationMenu();
+    }
+
+    System.out.print("Please enter voter count: ");
+    // try to parse to long and check if it is positive
+    // if not, display error message and ask again
+    // if yes, continue
+    String voterCount = sc.nextLine();
+    try {
+      long voterCountLong = Long.parseLong(voterCount);
+      if (voterCountLong <= 0) {
+        System.out.println("Voter count must be a positive integer.");
+        displayVotingSystemCreationMenu();
+      }
+      this.voterCount = voterCountLong;
+    } catch (NumberFormatException e) {
+      System.out.println("Voter count must be a positive integer.");
+      displayVotingSystemCreationMenu();
+    }
+
+    System.out.print("Please enter key pair bit length: ");
+    // try to parse to int and check if it is positive
+    // if not, display error message and ask again
+    // if yes, continue
+    String keyPairBitLength = sc.nextLine();
+    try {
+      int keyPairBitLengthInt = Integer.parseInt(keyPairBitLength);
+      if (keyPairBitLengthInt <= 0) {
+        System.out.println("Key pair bit length must be a positive integer.");
+        displayVotingSystemCreationMenu();
+      }
+      this.keyPairBitLength = keyPairBitLengthInt;
+    } catch (NumberFormatException e) {
+      System.out.println("Key pair bit length must be a positive integer.");
+      displayVotingSystemCreationMenu();
+    }
+
+    System.out.print("Please enter seed(0 for random seed): ");
+    // try to parse to long
+    // if not, display error message and ask again
+    // if yes, continue
+    String seed = sc.nextLine();
+    try {
+      long seedLong = Long.parseLong(seed);
+      this.seed = seedLong;
+    } catch (NumberFormatException e) {
+      System.out.println("Seed must be a long integer.");
+      displayVotingSystemCreationMenu();
+    }
+
+    // if seed is 0, create a random seed
+    if (this.seed == 0) {
+      this.seed = Math.abs(new Random().nextLong()) + 1;
+    }
+
+    // create a random object with the seed
+    this.random = new Random(this.seed);
+
+    // create the voting system
+    this.keyPair = PaillierCryptoSystem.generateKeyPair(this.keyPairBitLength, new SecureRandom());
+    this.candidates = createCandidates(this.candidateCount);
+    this.votingSystem = new VotingSystem(keyPair, this.seed, this.candidates, this.voterCount);
+    System.out.println("Voting system created successfully.");
+
+    // display the parameters
+    displayVotingSystemParameters();
+
+    displayVotingSystemMenu();
+  }
+
+  private void displayVotingSystemParameters() {
+    System.out.println("");
+    System.out.println("------------------------");
+    System.out.println("Voting System Parameters");
+    System.out.println("------------------------");
+
+    System.out.println("Candidate Count: " + candidateCount);
+    System.out.println("Voter Count: " + voterCount);
+    System.out.println("Key Pair Bit Length: " + keyPairBitLength);
+    System.out.println("Seed: " + seed);
+    System.out.println("");
+    System.out.println("Candidates: ");
+    System.out.println(candidates.stream().map(Candidate::toString).collect(Collectors.joining("\n")));
+    System.out.println("");
+    System.out.println("Public Key: " + keyPair.getPublicKey());
+    System.out.println("Private Key: " + keyPair.getPrivateKey());
+    System.out.println("------------------------");
+  }
+
+  private List<Candidate> createCandidates(long candidateCount) {
+    List<Candidate> candidates = new ArrayList<>();
+    for (int i = 0; i < candidateCount; i++) {
+      candidates.add(new Candidate(i, "Candidate " + i));
+    }
+    return candidates;
+  }
+
+  public void displayVotingSystemMenu() {
+    System.out.println("\n\nVoting System Menu");
+    System.out.println("1. Start Voting Randomly");
+    System.out.println("2. Display Results");
+    System.out.println("3. Display Voting System Parameters");
+    System.out.println("4. Return to Main Menu");
+    System.out.println("5. Exit");
+
+    System.out.print("Choice: ");
+    String choice = sc.nextLine();
+    switch (choice) {
+      case "1":
+        if (this.isVotingComplete) {
+          System.out.println("Already voted.");
+        }
+        startVotingRandomly();
+        break;
+      case "2":
+        if (!this.isVotingComplete) {
+          System.out.println("Voting is not complete yet.");
+          displayVotingSystemMenu();
+        }
+        displayResults();
+        break;
+      case "3":
+        displayVotingSystemParameters();
+        displayVotingSystemMenu();
+        break;
+      case "4":
+        displayMainMenu();
+        break;
+      case "5":
+        System.out.println("Exiting...");
+        System.exit(0);
+        break;
+      default:
+        System.out.println("Invalid choice. Please try again.");
+        displayVotingSystemMenu();
+    }
+  }
+
+  public void startVotingRandomly() {
+    System.out.println("\nStarting voting randomly...");
+    for (int i = 0; i < this.voterCount; i++) {
+      try {
+        int candidateIndex = this.random.nextInt((int) this.candidateCount);
+        System.out.println("Voter " + i + " voting for: " + this.candidates.get(candidateIndex).getName());
+        this.votingSystem.addVote(candidateIndex);
+      } catch (UnknownCandidateException e) {
+        System.out.println("Unknown candidate exception occured.");
+        break;
+      }
+    }
+    System.out.println("Voting completed successfully.");
+    this.isVotingComplete = true;
+    displayVotingSystemMenu();
+  }
+
+  public void displayResults() {
+    List<Candidate> result = this.votingSystem.tallyVotes();
+    System.out.println("Votes tallied successfully.");
+
+    List<Candidate> sorted = result.stream()
+        .sorted((a, b) -> Long.compare(b.getVotes(), a.getVotes()))
+        .collect(Collectors.toList());
+
+    System.out.println("Results");
+    for (Candidate candidate : sorted) {
+      System.out.println(candidate);
+    }
+    displayVotingSystemMenu();
+  }
+}
